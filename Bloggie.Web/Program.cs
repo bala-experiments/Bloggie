5d8f7008aaf26b9306da79504a8d68ab0a4a8039
balajayapal <bala.kodis@gmail.com>
2023-10-30 14:04:12 +0530

    Comments updated

--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -5,29 +5,28 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+/* Start Adding Services after creation of WebApp Builder object*/
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 // Add BloggieDb Service
 builder.Services.AddDbContext<BloggieDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString"))
     );
-
 //Adding BloggieAuthDb service
 builder.Services.AddDbContext<AuthDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieAuthDbConnectionString"))
     );
-
 //Registering Identity
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<AuthDbContext>();
-
 builder.Services.AddScoped<ITagRepository,TagRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
 builder.Services.AddScoped<IImageRepository, CloudinaryImageRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+/* Ending services */
 
+
+//Creating Web App using Builder object
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -35,16 +34,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
